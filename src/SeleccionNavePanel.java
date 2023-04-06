b7325a00f31555f393dbb4e51bc0e784352d90d4
Canela <carpinteron@uninorte.edu.co>
2023-04-06 15:59:17 -0500

    WIiiooo

---
+++ src/SeleccionNavePanel.java
@@ -0,0 +1,82 @@
+
+public class SeleccionNavePanel extends javax.swing.JPanel {
+
+    public SeleccionNavePanel() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel3 = new javax.swing.JLabel();
+        NARANJAcBTN = new javax.swing.JButton();
+        AZULcBTN = new javax.swing.JButton();
+        VERDEcBTN = new javax.swing.JButton();
+
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setFont(new java.awt.Font("Berlin Sans FB Demi", 2, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(153, 153, 255));
+        jLabel3.setText("Elige la nave que desees...");
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 10, 306, 60));
+
+        NARANJAcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/naranjacarro.png"))); // NOI18N
+        NARANJAcBTN.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        NARANJAcBTN.setBorderPainted(false);
+        NARANJAcBTN.setContentAreaFilled(false);
+        NARANJAcBTN.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        NARANJAcBTN.setFocusPainted(false);
+        NARANJAcBTN.setFocusable(false);
+        NARANJAcBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NARANJAcBTNActionPerformed(evt);
+            }
+        });
+        add(NARANJAcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(33, 81, 250, 150));
+
+        AZULcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/axullcarro.png"))); // NOI18N
+        AZULcBTN.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        AZULcBTN.setBorderPainted(false);
+        AZULcBTN.setContentAreaFilled(false);
+        AZULcBTN.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        AZULcBTN.setFocusPainted(false);
+        AZULcBTN.setFocusable(false);
+        AZULcBTN.setRequestFocusEnabled(false);
+        AZULcBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AZULcBTNActionPerformed(evt);
+            }
+        });
+        add(AZULcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 80, -1, 150));
+
+        VERDEcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/verdecarro (2)_1.png"))); // NOI18N
+        VERDEcBTN.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        VERDEcBTN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                VERDEcBTNActionPerformed(evt);
+            }
+        });
+        add(VERDEcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(161, 243, -1, -1));
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void NARANJAcBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NARANJAcBTNActionPerformed
+        
+    }//GEN-LAST:event_NARANJAcBTNActionPerformed
+
+    private void AZULcBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AZULcBTNActionPerformed
+        
+    }//GEN-LAST:event_AZULcBTNActionPerformed
+
+    private void VERDEcBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VERDEcBTNActionPerformed
+     
+    }//GEN-LAST:event_VERDEcBTNActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton AZULcBTN;
+    private javax.swing.JButton NARANJAcBTN;
+    private javax.swing.JButton VERDEcBTN;
+    private javax.swing.JLabel jLabel3;
+    // End of variables declaration//GEN-END:variables
+}
