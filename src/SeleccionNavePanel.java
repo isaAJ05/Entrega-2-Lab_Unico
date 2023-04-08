ec69da76e0ad1f8e3a4e25082a5ebb84635c04f2
Canela <carpinteron@uninorte.edu.co>
2023-04-08 14:17:10 -0500

    Saben como hacer para seleccionar un personaje ?

--- src/SeleccionNavePanel.java
+++ src/SeleccionNavePanel.java
@@ -5,78 +5,60 @@ public class SeleccionNavePanel extends javax.swing.JPanel {
         initComponents();
     }
 
+   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel3 = new javax.swing.JLabel();
-        NARANJAcBTN = new javax.swing.JButton();
-        AZULcBTN = new javax.swing.JButton();
-        VERDEcBTN = new javax.swing.JButton();
+        AzulcBTN = new javax.swing.JButton();
+        NaranjacBTN = new javax.swing.JButton();
+        VerdecBTN = new javax.swing.JButton();
 
+        setOpaque(false);
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel3.setFont(new java.awt.Font("Berlin Sans FB Demi", 2, 24)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(153, 153, 255));
-        jLabel3.setText("Elige la nave que desees...");
-        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 10, 306, 60));
-
-        NARANJAcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/naranjacarro.png"))); // NOI18N
-        NARANJAcBTN.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        NARANJAcBTN.setBorderPainted(false);
-        NARANJAcBTN.setContentAreaFilled(false);
-        NARANJAcBTN.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        NARANJAcBTN.setFocusPainted(false);
-        NARANJAcBTN.setFocusable(false);
-        NARANJAcBTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                NARANJAcBTNActionPerformed(evt);
+        AzulcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/axullcarro.png"))); // NOI18N
+        AzulcBTN.setBorderPainted(false);
+        AzulcBTN.setContentAreaFilled(false);
+        AzulcBTN.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        AzulcBTN.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                AzulcBTNFocusGained(evt);
             }
         });
-        add(NARANJAcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(33, 81, 250, 150));
+        add(AzulcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 130, 190, 120));
 
-        AZULcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/axullcarro.png"))); // NOI18N
-        AZULcBTN.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        AZULcBTN.setBorderPainted(false);
-        AZULcBTN.setContentAreaFilled(false);
-        AZULcBTN.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        AZULcBTN.setFocusPainted(false);
-        AZULcBTN.setFocusable(false);
-        AZULcBTN.setRequestFocusEnabled(false);
-        AZULcBTN.addActionListener(new java.awt.event.ActionListener() {
+        NaranjacBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/naranjacarro.png"))); // NOI18N
+        NaranjacBTN.setBorderPainted(false);
+        NaranjacBTN.setContentAreaFilled(false);
+        NaranjacBTN.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        NaranjacBTN.setFocusPainted(false);
+        NaranjacBTN.setFocusable(false);
+        NaranjacBTN.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AZULcBTNActionPerformed(evt);
+                NaranjacBTNActionPerformed(evt);
             }
         });
-        add(AZULcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 80, -1, 150));
+        add(NaranjacBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 120, 190, 120));
 
-        VERDEcBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/verdecarro (2)_1.png"))); // NOI18N
-        VERDEcBTN.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        VERDEcBTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                VERDEcBTNActionPerformed(evt);
-            }
-        });
-        add(VERDEcBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(161, 243, -1, -1));
+        VerdecBTN.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGcars/verdecarro (2)_1.png"))); // NOI18N
+        VerdecBTN.setBorderPainted(false);
+        VerdecBTN.setContentAreaFilled(false);
+        add(VerdecBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 120, 190, 120));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void NARANJAcBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NARANJAcBTNActionPerformed
-        
-    }//GEN-LAST:event_NARANJAcBTNActionPerformed
+    private void NaranjacBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NaranjacBTNActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_NaranjacBTNActionPerformed
 
-    private void AZULcBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AZULcBTNActionPerformed
-        
-    }//GEN-LAST:event_AZULcBTNActionPerformed
-
-    private void VERDEcBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VERDEcBTNActionPerformed
-     
-    }//GEN-LAST:event_VERDEcBTNActionPerformed
+    private void AzulcBTNFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_AzulcBTNFocusGained
+        AzulcBTN.setText("hiiiiiiiii");
+    }//GEN-LAST:event_AzulcBTNFocusGained
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton AZULcBTN;
-    private javax.swing.JButton NARANJAcBTN;
-    private javax.swing.JButton VERDEcBTN;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JButton AzulcBTN;
+    private javax.swing.JButton NaranjacBTN;
+    private javax.swing.JButton VerdecBTN;
     // End of variables declaration//GEN-END:variables
 }
