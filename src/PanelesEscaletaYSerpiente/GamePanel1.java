fae89d41341ce3c368ba4950285e90d339e85cb3
Canela <carpinteron@uninorte.edu.co>
2023-04-10 01:45:13 -0500

    El de escalera en proceso Hice el Timer falta los otros dos carros en el de carrera

---
+++ src/PanelesEscaletaYSerpiente/GamePanel1.java
@@ -0,0 +1,101 @@
+package PanelesEscaletaYSerpiente;
+
+import java.util.Random;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GamePanel1 extends javax.swing.JPanel {
+
+    private Tablero1 matriz = new Tablero1();
+
+    Tablero1 insta = new Tablero1();
+    public int casilla = insta.casilla;
+
+    public static int dado;
+
+    public GamePanel1() {
+        initComponents();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        TirarDados = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        NumDado = new javax.swing.JLabel();
+        AvatarU = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(0, 0, 51));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGescaleraSerp/Eldado.png"))); // NOI18N
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(628, 18, 108, 104));
+
+        TirarDados.setText("Tirar");
+        TirarDados.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        TirarDados.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TirarDadosActionPerformed(evt);
+            }
+        });
+        add(TirarDados, new org.netbeans.lib.awtextra.AbsoluteConstraints(645, 134, -1, -1));
+
+        jLabel2.setText("jLabel2");
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 260, 70, 70));
+
+        NumDado.setFont(new java.awt.Font("Swis721 Ex BT", 1, 18)); // NOI18N
+        NumDado.setForeground(new java.awt.Color(204, 204, 255));
+        NumDado.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        NumDado.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        add(NumDado, new org.netbeans.lib.awtextra.AbsoluteConstraints(645, 174, 72, 46));
+
+        AvatarU.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGescaleraSerp/AlienVerdeMini.png"))); // NOI18N
+        add(AvatarU, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 310, 50, 70));
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IMGescaleraSerp/fondo serp 1.png"))); // NOI18N
+        add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+    }// </editor-fold>//GEN-END:initComponents
+
+    Random ran = new Random();
+    private void TirarDadosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TirarDadosActionPerformed
+        NumDado.setText("");
+        dado = ran.nextInt(6) + 1;
+        NumDado.setText(Integer.toString(dado));
+
+        int[][] Tablero = matriz.Hacerespaciopista();
+
+
+    }//GEN-LAST:event_TirarDadosActionPerformed
+    public class accion {
+
+        Timer timer = new Timer(1, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                repaint();
+            }
+        });
+
+        public accion() {
+            timer.start();
+        }
+
+        protected void Ficha() {
+            
+        }
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel AvatarU;
+    private javax.swing.JLabel Fondo;
+    private javax.swing.JLabel NumDado;
+    private javax.swing.JButton TirarDados;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    // End of variables declaration//GEN-END:variables
+}
