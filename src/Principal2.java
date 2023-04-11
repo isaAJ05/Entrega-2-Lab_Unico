b4f088db4fead26d59895cc5fcb80c758df51364
Canela <carpinteron@uninorte.edu.co>
2023-04-10 22:40:23 -0500

    djjkw

--- src/Principal2.java
+++ src/Principal2.java
@@ -21,6 +21,7 @@ public class Principal2 extends javax.swing.JFrame {
         OK = new javax.swing.JButton();
         labeluser = new javax.swing.JLabel();
         volver = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -31,6 +32,7 @@ public class Principal2 extends javax.swing.JFrame {
                 panelKeyPressed(evt);
             }
         });
+        panel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         nombre.setBackground(new java.awt.Color(102, 102, 255));
         nombre.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
@@ -39,6 +41,7 @@ public class Principal2 extends javax.swing.JFrame {
                 nombreKeyTyped(evt);
             }
         });
+        panel.add(nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 290, 210, 40));
 
         OK.setBackground(new java.awt.Color(0, 0, 102));
         OK.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
@@ -48,9 +51,11 @@ public class Principal2 extends javax.swing.JFrame {
                 OKActionPerformed(evt);
             }
         });
+        panel.add(OK, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 290, -1, 40));
 
         labeluser.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
         labeluser.setForeground(new java.awt.Color(255, 0, 0));
+        panel.add(labeluser, new org.netbeans.lib.awtextra.AbsoluteConstraints(522, 443, -1, -1));
 
         volver.setBackground(new java.awt.Color(0, 0, 102));
         volver.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
@@ -60,44 +65,10 @@ public class Principal2 extends javax.swing.JFrame {
                 volverActionPerformed(evt);
             }
         });
+        panel.add(volver, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, -1, -1));
 
-        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
-        panel.setLayout(panelLayout);
-        panelLayout.setHorizontalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelLayout.createSequentialGroup()
-                        .addComponent(volver)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
-                        .addGap(0, 516, Short.MAX_VALUE)
-                        .addComponent(labeluser)
-                        .addGap(278, 278, 278))))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(OK)
-                .addGap(73, 73, 73))
-        );
-        panelLayout.setVerticalGroup(
-            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(panelLayout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(nombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(OK)))
-                    .addGroup(panelLayout.createSequentialGroup()
-                        .addComponent(volver)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 411, Short.MAX_VALUE)
-                        .addComponent(labeluser)))
-                .addGap(57, 57, 57))
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/general/stella (2).png"))); // NOI18N
+        panel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 458, 286));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -153,52 +124,52 @@ public class Principal2 extends javax.swing.JFrame {
         super.paint(g);
         Graphics gr = panel.getGraphics();
 
-        gr.setColor(Color.yellow);
-        int[] vx = {60, 123, 150, 177, 240, 190, 200, 150, 100, 110};
-        int[] vy = {160, 160, 100, 160, 160, 200, 260, 220, 260, 200};
-        gr.fillPolygon(vx, vy, 10);
-
-        gr.setColor(Color.red);
-        int[] xt1 = {150, 123, 177};
-        int[] yt1 = {100, 160, 160};
-        gr.fillPolygon(xt1, yt1, 3);
-
-        gr.setColor(Color.green);
-        int[] xt2 = {123, 60, 110};
-        int[] yt2 = {160, 160, 200};
-        gr.fillPolygon(xt2, yt2, 3);
-
-        gr.setColor(Color.magenta);
-        int[] xt3 = {177, 240, 190};
-        int[] yt3 = {160, 160, 200};
-        gr.fillPolygon(xt3, yt3, 3);
-
-        gr.setColor(Color.blue);
-        int[] xt4 = {110, 150, 100};
-        int[] yt4 = {200, 220, 260};
-        gr.fillPolygon(xt4, yt4, 3);
-
-        gr.setColor(Color.cyan);
-        int[] xt5 = {190, 150, 200};
-        int[] yt5 = {200, 220, 260};
-        gr.fillPolygon(xt5, yt5, 3);
+//        gr.setColor(Color.yellow);
+//        int[] vx = {60, 123, 150, 177, 240, 190, 200, 150, 100, 110};
+//        int[] vy = {160, 160, 100, 160, 160, 200, 260, 220, 260, 200};
+//        gr.fillPolygon(vx, vy, 10);
+//
+//        gr.setColor(Color.red);
+//        int[] xt1 = {150, 123, 177};
+//        int[] yt1 = {100, 160, 160};
+//        gr.fillPolygon(xt1, yt1, 3);
+//
+//        gr.setColor(Color.green);
+//        int[] xt2 = {123, 60, 110};
+//        int[] yt2 = {160, 160, 200};
+//        gr.fillPolygon(xt2, yt2, 3);
+//
+//        gr.setColor(Color.magenta);
+//        int[] xt3 = {177, 240, 190};
+//        int[] yt3 = {160, 160, 200};
+//        gr.fillPolygon(xt3, yt3, 3);
+//
+//        gr.setColor(Color.blue);
+//        int[] xt4 = {110, 150, 100};
+//        int[] yt4 = {200, 220, 260};
+//        gr.fillPolygon(xt4, yt4, 3);
+//
+//        gr.setColor(Color.cyan);
+//        int[] xt5 = {190, 150, 200};
+//        int[] yt5 = {200, 220, 260};
+//        gr.fillPolygon(xt5, yt5, 3);
 
         gr.setColor(Color.orange);
         Font f3 = new Font("Engravers MT", Font.BOLD, 25);
         gr.setFont(f3);
         gr.drawString("STAR Games" + "", 310, 100);
 
-        g.fillOval(141, 200, 8, 13);
-        g.fillOval(167, 200, 8, 13);
-
-        gr.setColor(Color.white);
-        gr.fillOval(136, 170, 3, 3);
-        gr.fillOval(162, 170, 3, 3);
-
-        gr.setColor(Color.black);
-        g.fillOval(141, 217, 35, 20);
-        gr.setColor(Color.yellow);
-        gr.fillRect(133, 185, 35, 9);
+//        g.fillOval(141, 200, 8, 13);
+//        g.fillOval(167, 200, 8, 13);
+//
+//        gr.setColor(Color.white);
+//        gr.fillOval(136, 170, 3, 3);
+//        gr.fillOval(162, 170, 3, 3);
+//
+//        gr.setColor(Color.black);
+//        g.fillOval(141, 217, 35, 20);
+//        gr.setColor(Color.yellow);
+//        gr.fillRect(133, 185, 35, 9);
 
         gr.setColor(Color.white);
         Font f2 = new Font("Monospaced", Font.PLAIN, 18);
@@ -252,6 +223,7 @@ public class Principal2 extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton OK;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel labeluser;
     private javax.swing.JTextField nombre;
     private javax.swing.JPanel panel;
